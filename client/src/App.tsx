11cb252b76c7a5ba298645f922fe5c601ff49bac
ana sofia zeballos <azeballos512@gmail.com>
2025-07-18 15:08:03 -0300

    Reparando el link para verlo en la nube

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 // client/src/App.tsx
 import { Switch, Route } from "wouter";
 import NotFound from "@/pages/not-found";
+import HomePage from "@/pages/home-page";
 
 export default function App() {
   return (
